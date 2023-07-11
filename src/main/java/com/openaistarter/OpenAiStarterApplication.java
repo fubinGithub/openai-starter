6b0ebb867ec202b479821f85e596651b5c40281d
zhenfubin <13191565+zhenfubin@user.noreply.gitee.com>
2023-07-11 19:39:12 +0800

    init project

---
+++ src/main/java/com/openaistarter/OpenAiStarterApplication.java
@@ -0,0 +1,13 @@
+package com.openaistarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiStarterApplication.class, args);
+    }
+
+}
